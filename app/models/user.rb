bb6350edaf8839af363625581526b6edfe5139a8
jiheekwon <konn3444@gmail.com>
2022-03-09 18:38:37 +0900

    単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,        presence: true
-  validates :last_name,       presence: true
-  validates :first_name,      presence: true
-  validates :last_name_kana,  presence: true
-  validates :first_name_kana, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :last_name,       presence: true
+    validates :first_name,      presence: true
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヴー]+\z/u } do
+    validates :last_name_kana,  presence: true
+    validates :first_name_kana, presence: true
+  end
+
   validates :birth,           presence: true
 end
+
---
+++ spec/factories/users.rb
@@ -0,0 +1,19 @@
+FactoryBot.define do
+  factory :user do
+
+    transient do
+      person { Gimei.name }
+    end
+
+    nickname {Faker::Name.name}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    last_name {person.last}
+    first_name {person.first}
+    last_name_kana {person.last.katakana}
+    first_name_kana {person.first.katakana}
+    birth {Faker::Date.backward}
+  end
+end
