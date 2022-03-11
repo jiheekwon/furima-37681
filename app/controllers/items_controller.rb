74c83f0b66e0c503c6f7586ae5c25b6166909413
jiheekwon <konn3444@gmail.com>
2022-03-11 18:14:17 +0900

    Revert "商品編集機能"
    This reverts commit 10183b0893bc330d0334e044b081bd4af911000e.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -23,30 +22,10 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def edit
-    @item = Item.find(params[:id])
-  end
-
-  def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
-      redirect_to item_path(@item.id)
-    else
-      render :edit
-    end
-  end
-
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :explain, :category_id, :condition_id, :burden_id, :prefecture_id,
                                  :shipping_date_id, :price).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    item = Item.find(params[:id])
-    unless current_user.id == item.user_id
-      redirect_to action: :index
-    end
   end
 end
